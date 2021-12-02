9065653c36df9822853b8d6963b03e60707a809c
XiaoDong <qiuxiaod@gmail.com>
2021-12-02 23:08:01 +0800

    update 20211202

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from  './style';
 import { renderRoutes } from 'react-router-config';
 import routes from './routes';
+import store from './store';
 import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
-      <IconStyle></IconStyle>
-      <GlobalStyle></GlobalStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <IconStyle></IconStyle>
+        <GlobalStyle></GlobalStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
