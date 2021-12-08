7d6a9b6fa9b1980ffbf7368ac3fa276a70296380
XiaoDong <qiuxiaod@gmail.com>
2021-12-08 23:28:16 +0800

    更新轮播组件

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Slider from '../../components/slider';
 
 function Recommend() {
-    return <h1>Recommend</h1>;
+    const bannerList = new Array(5).fill({
+        imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg"
+    });
+
+    return <div>
+        <Slider bannerList={bannerList} />
+    </div>;
 }
 
 export default React.memo(Recommend);
